b49ad7672d5fe387dd661277cead1d6a73b20399
zhangjian <471362807@qq.com>
2023-09-19 10:37:32 +0800

    创建springboot

---
+++ src/main/java/com/Application.java
@@ -0,0 +1,19 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Date:2023-09-19-10:27
+ *
+ * @author zhangjian
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+
